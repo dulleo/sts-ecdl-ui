c09a50fe57f4fcce547e828faafea86d0355fd63
duskol <dulleo@gmail.com>
2018-11-04 13:26:19 +0100

    routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateTestComponent } from './create-test/create-test.component';
+import { TestListComponent } from './test-list/test-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'tests', pathMatch: 'full' },
+  { path: 'tests', component: TestListComponent},
+  { path: 'tests/create', component: CreateTestComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
