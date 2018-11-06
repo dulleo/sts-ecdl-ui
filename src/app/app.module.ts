a24e7c680607d93545a9bbdc97086463ef29c9c7
duskol <dulleo@gmail.com>
2018-11-06 13:22:22 +0100

    edit-test, create-question, create-answers component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,18 +8,29 @@ import { DataService } from './service/data.service';
 import { HttpClientModule } from '@angular/common/http'; 
 import { CreateTestComponent } from './create-test/create-test.component';
 import { FormsModule } from '@angular/forms';
+import { MessageService } from './service/message.service';
+import { EditTestComponent } from './edit-test/edit-test.component';
+import { CreateQuestionComponent } from './create-question/create-question.component';
+import { CommonModule } from '@angular/common';
+import { CreateAnswersComponent } from './answers/create-answers/create-answers.component';
 
 @NgModule({
   declarations: [
-    AppComponent, TestListComponent, CreateTestComponent
+    AppComponent, 
+    TestListComponent, 
+    CreateTestComponent, 
+    EditTestComponent,
+    CreateQuestionComponent,
+    CreateAnswersComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    CommonModule
   ],
-  providers: [DataService],
+  providers: [DataService, MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
