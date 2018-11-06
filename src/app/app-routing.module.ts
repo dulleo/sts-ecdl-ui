7d84122b168982ffef620794d236f28a36b49c72
duskol <dulleo@gmail.com>
2018-11-06 13:22:39 +0100

    route added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CreateTestComponent } from './create-test/create-test.component';
 import { TestListComponent } from './test-list/test-list.component';
+import { EditTestComponent } from './edit-test/edit-test.component';
+import { CreateQuestionComponent } from './create-question/create-question.component';
+import { CreateAnswersComponent } from './answers/create-answers/create-answers.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'tests', pathMatch: 'full' },
   { path: 'tests', component: TestListComponent},
-  { path: 'tests/create', component: CreateTestComponent}
+  { path: 'tests/create', component: CreateTestComponent},
+  { path: 'tests/edit', component: EditTestComponent},
+  { path: 'questions/create', component: CreateQuestionComponent },
+  { path: 'answers/create', component: CreateAnswersComponent }
 ];
 
 @NgModule({
