ebce642f2a87d0b012778f30ddc1e7c87a7597fe
duskol <dulleo@gmail.com>
2018-11-06 13:18:32 +0100

    edit, delet test and create question added

--- src/app/service/data.service.ts
+++ src/app/service/data.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from "src/environments/environment";
 import { Test } from "../model/test.model";
+import { Question } from "../model/question.model";
 
 
 @Injectable()
@@ -16,6 +17,7 @@ export class DataService {
     }
 
     getTests() {
+        console.log("[Service] --> Get tests")
         return this.httpClient.get(this.baseUrl + '/tests', {
             headers: new HttpHeaders().set('Content-Type', 'application/json').set('Accept', '*'),
             observe: 'response'
@@ -23,6 +25,7 @@ export class DataService {
     }
 
     createTest(test: Test) {
+        console.log("[Service] --> Create test")
         const body =JSON.stringify(test);
         return this.httpClient.post(this.baseUrl + '/tests', body,{
             headers: new HttpHeaders().set('Content-Type', 'application/json')
@@ -31,4 +34,34 @@ export class DataService {
         })
     }
 
+    deleteTest(id:number) {
+        console.log("[Service] --> Delete test id: " + id)
+        let deleteUrl: string;
+        deleteUrl = this.baseUrl + '/tests/' + id;
+        return this.httpClient.delete(deleteUrl, {
+            headers: new HttpHeaders().set('Content-Type', 'application/json').set('Accept', '*'),
+            observe: 'response'
+        })
+    }
+
+    editTest(test: Test) {
+        const body =JSON.stringify(test);
+        return this.httpClient.put(this.baseUrl + '/tests', body, {
+            headers: new HttpHeaders().set('Content-Type', 'application/json').set('Accept', '*'),
+            observe: 'response'
+        })
+    }
+
+    //******************************** */
+    createQuestion(question: Question) {
+        console.log("[Service] --> Create question for test: " + question.getTestId());
+        console.log("[Service] --> Create question: " + question.getText());
+        const body =JSON.stringify(question);
+        console.log("[Service] --> Create question body: " + body);
+        return this.httpClient.post(this.baseUrl + '/questions', body,{
+            headers: new HttpHeaders().set('Content-Type', 'application/json')
+                                        .set('Accept', '*'),
+            observe: 'response'
+        })
+    }
 }
