6a819c21af7ad45f558920bb4bce5db674d2a41a
duskol <dulleo@gmail.com>
2018-12-15 00:59:10 +0100

    exam test message service added

--- src/app/service/message.service.ts
+++ src/app/service/message.service.ts
@@ -2,24 +2,32 @@ import { Injectable } from "@angular/core";
 import { BehaviorSubject } from "rxjs";
 import { Test } from "../model/test.model";
 import { Question } from "../model/question.model";
+import { ExamDTO } from "../modelDto/exam.dto";
+import { TestDTO } from "../modelDto/test.dto";
 
 @Injectable()
 export class MessageService {
 
-    private testMessageSource = new BehaviorSubject(new Test);
+    private testMessageSource = new BehaviorSubject(new TestDTO);
     selectedTest = this.testMessageSource.asObservable();
 
     private questionMessageSource = new BehaviorSubject(new Question);
     selectedQuestion = this.questionMessageSource.asObservable();
 
+    private examTestMessageSource = new BehaviorSubject(new ExamDTO);
+    examTest = this.examTestMessageSource.asObservable();
+
     constructor() {}
 
-    changeTest(test: Test) {
-        this.testMessageSource.next(test);
+    selectTest(testDTO: TestDTO) {
+        this.testMessageSource.next(testDTO);
     }
 
-    changeQuestion(question: Question) {
+    selectQuestion(question: Question) {
         this.questionMessageSource.next(question);
     }
 
+    selectExamTest(examTest: ExamDTO) {
+        this.examTestMessageSource.next(examTest);
+    }
 }
