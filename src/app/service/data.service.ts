e2905e0d8668683d7ab7f1500fcb05a1dec2d424
duskol <dulleo@gmail.com>
2018-12-15 00:59:48 +0100

    exam test

--- src/app/service/data.service.ts
+++ src/app/service/data.service.ts
@@ -3,6 +3,12 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from "src/environments/environment";
 import { Test } from "../model/test.model";
 import { Question } from "../model/question.model";
+import { Answer } from "../model/answer.model";
+//import { ExamAnswerDto } from "../modelDto/exam.answer.dto";
+//import { ExamTestDto } from "../modelDto/exam.test.dto";
+import { TestDTO } from "../modelDto/test.dto";
+import { ExamTestDTO } from "../modelDto/exam.test.dto";
+import { ExamDTO } from "../modelDto/exam.dto";
 
 
 @Injectable()
@@ -17,16 +23,25 @@ export class DataService {
     }
 
     getTests() {
-        console.log("[Service] --> Get tests")
+        console.log("[Service] --> Get tests");
         return this.httpClient.get(this.baseUrl + '/tests', {
             headers: new HttpHeaders().set('Content-Type', 'application/json').set('Accept', '*'),
             observe: 'response'
         })
     }
 
-    createTest(test: Test) {
-        console.log("[Service] --> Create test")
-        const body =JSON.stringify(test);
+    getTest(testId: number) {
+        console.log("[Service] --> Get test");
+        return this.httpClient.get(this.baseUrl + '/tests/' + testId, {
+            headers: new HttpHeaders().set('Content-Type', 'application/json').set('Accept', '*'),
+            observe: 'response'
+        })
+    }
+
+    createTest(testDTO: TestDTO) {
+        console.log("[Service] --> Create test");
+        const body =JSON.stringify(testDTO);
+        console.log("[Service] --> body: " + body);
         return this.httpClient.post(this.baseUrl + '/tests', body,{
             headers: new HttpHeaders().set('Content-Type', 'application/json')
                                         .set('Accept', '*'),
@@ -44,8 +59,8 @@ export class DataService {
         })
     }
 
-    editTest(test: Test) {
-        const body =JSON.stringify(test);
+    editTest(testDTO: TestDTO) {
+        const body =JSON.stringify(testDTO);
         return this.httpClient.put(this.baseUrl + '/tests', body, {
             headers: new HttpHeaders().set('Content-Type', 'application/json').set('Accept', '*'),
             observe: 'response'
@@ -53,14 +68,44 @@ export class DataService {
     }
 
     //******************************** */
-    createQuestion(question: Question) {
-        console.log("[Service] --> Create question for test: " + question.getTestId());
-        console.log("[Service] --> Create question: " + question.getText());
+    createQuestion(testId:number, question: Question) {
+        console.log("[Service] --> Create question for test: " + testId);
+        console.log("[Service] --> Create question: " + question.Text);
         const body =JSON.stringify(question);
         console.log("[Service] --> Create question body: " + body);
-        return this.httpClient.post(this.baseUrl + '/questions', body,{
+        return this.httpClient.post(this.baseUrl + '/tests' + testId + '/questions', body,{
             headers: new HttpHeaders().set('Content-Type', 'application/json')
                                         .set('Accept', '*'),
+            observe: 'response'
+        })
+    }
+
+    /* ANSWER SECTION */
+    createAnswers(questionId: number, answers: Array<Answer>) {
+        console.log("[Service] --> Create answers for question: " + questionId);
+        const body =JSON.stringify(answers);
+        console.log("[Service] --> Create aswers body: " + body);
+        return this.httpClient.post(this.baseUrl + '/questions/' + questionId +  '/answers', body,{
+            headers: new HttpHeaders().set('Content-Type', 'application/json')
+                                        .set('Accept', '*'),
+            observe: 'response'
+        })
+    }
+
+    /* EXAM */
+    getExamTest(testId: number) {
+        console.log("[Service] --> Get exam test id: " + testId);
+        return this.httpClient.get(this.baseUrl + '/exams/' + testId, {
+            headers: new HttpHeaders().set('Content-Type', 'application/json').set('Accept', '*'),
+            observe: 'response'
+        })
+    }
+
+    submitTest(examTest: ExamDTO) {
+        //console.log("[Service] --> Submit test id: " + examTest.Id);
+        const body =JSON.stringify(examTest);
+        return this.httpClient.put(this.baseUrl + '/exams/', body, {
+            headers: new HttpHeaders().set('Content-Type', 'application/json').set('Accept', '*'),
             observe: 'response'
         })
     }
