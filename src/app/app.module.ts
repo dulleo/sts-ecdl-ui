9802b90cba620d600858b5aa3b496d6e053a5f7f
duskol <dulleo@gmail.com>
2019-01-05 13:04:22 +0100

    removed deleted component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,32 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TestListComponent } from './test-list/test-list.component';
+import { TestListComponent } from './test/list/test-list.component';
 import { DataService } from './service/data.service';
 import { HttpClientModule } from '@angular/common/http'; 
-import { CreateTestComponent } from './create-test/create-test.component';
 import { FormsModule } from '@angular/forms';
 import { MessageService } from './service/message.service';
-import { EditTestComponent } from './edit-test/edit-test.component';
-import { CreateQuestionComponent } from './create-question/create-question.component';
 import { CommonModule } from '@angular/common';
-import { CreateAnswersComponent } from './answers/create-answers/create-answers.component';
+import { ExamComponent } from './exam/exam.component';
+import { ReviewTestComponent } from './test/review/review-test.component';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { QuestionListComponent } from './question/list/question-list.component';
+import { QuestionManageComponent } from './question/manage/question-manage.component';
+import { TestManageComponent } from './test/manage/test-manage.component';
 
 @NgModule({
   declarations: [
     AppComponent, 
-    TestListComponent, 
-    CreateTestComponent, 
-    EditTestComponent,
-    CreateQuestionComponent,
-    CreateAnswersComponent
+    TestListComponent,
+    ExamComponent,
+    TestManageComponent,
+    ReviewTestComponent,
+    QuestionListComponent,
+    QuestionManageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    CommonModule
+    CommonModule,
+    BsDropdownModule.forRoot()
   ],
   providers: [DataService, MessageService],
   bootstrap: [AppComponent]
